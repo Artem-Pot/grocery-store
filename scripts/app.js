a604760a7707115e4dd9640f2c66db8146360721
Artem <blbo@yandex.ru>
2024-03-27 16:38:38 +0400

    0.0.4
    Начало работы со слайдером

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper('.swiper', {
+    loop: true, //зацикливание слайдов до бесконечности
+    grabCursor: true, //изменит курсор для перетаскивания
+    speed: 1500, //скорость переключения слайда
+ 
+    effect: 'fade', //эффект прозрачности при смене слайдов
+    fadeEffect: {
+        crossFade: true, //дополнение к эффекту fade - параллельная смена прозрачности, добавляет плавность
+    },
+  
+    direction: 'horizontal', //горизонтальный слайдер
+
+    pagination: {     // нумерация страниц
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {     // Кнопки навигации
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {    // Полоса прокрутки
+      el: '.swiper-scrollbar',
+    },
+
+    autoplay: {
+        delay: 1000, //паузка между прокруткой
+        disableOnInteraction: true, // отключить при ручном переключении
+    },
+
+    keyboard: { //прокрутка с помощью клавиатуры
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+  });
