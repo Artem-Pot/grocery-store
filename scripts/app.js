806118a5cb0a867c30bfe7807cf98ddd7b30bf15
Artem <blbo@yandex.ru>
2024-03-29 13:36:43 +0400

    0.0.8
    Специальные предложения блок.

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,6 @@
 const swiper = new Swiper('.swiper', {
     loop: true, //зацикливание слайдов до бесконечности
-    grabCursor: true, //изменит курсор для перетаскивания
-    speed: 1500, //скорость переключения слайда
+    speed: 1000, //скорость переключения слайда
  
     effect: 'fade', //эффект прозрачности при смене слайдов
     fadeEffect: {
@@ -10,27 +9,8 @@ const swiper = new Swiper('.swiper', {
   
     direction: 'horizontal', //горизонтальный слайдер
 
-    pagination: {     // нумерация страниц
-      el: '.swiper-pagination',
-    },
-  
-    navigation: {     // Кнопки навигации
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    scrollbar: {    // Полоса прокрутки
-      el: '.swiper-scrollbar',
-    },
-
     autoplay: {
-        delay: 1000, //паузка между прокруткой
+        delay: 3000, //паузка между прокруткой
         disableOnInteraction: true, // отключить при ручном переключении
     },
-
-    keyboard: { //прокрутка с помощью клавиатуры
-      enabled: true,
-      onlyInViewport: false,
-    },
-
   });
